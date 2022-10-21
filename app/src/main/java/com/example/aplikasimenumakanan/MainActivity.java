b40b7cf3f74ac2a7cbb2c78b8bf4094e9773939d
Dimas Pratama <yohanesdimas888@gmail.com>
2022-10-21 22:56:38 +0700

    Update

--- app/src/main/java/com/example/aplikasimenumakanan/MainActivity.java
+++ app/src/main/java/com/example/aplikasimenumakanan/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         this.listKuliner = new ArrayList<>();
         listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Lunpia", "Makanan khas Jawa Tengah", "Rp. 40.000", R.drawable.lunpia));
         listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Rujak Cingur", "Makanan khas Jawa Timur", "Rp. 15.000", R.drawable.rujak_cingur));
-        listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Karedok", "Makanan khas jawa Barat", "Rp. 10.000", R.drawable.karedok));
+        listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Karedok", "Makanan khas Jawa Barat", "Rp. 10.000", R.drawable.karedok));
         listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Gudeg", "Makanan khas Yogyakarta", "Rp. 50.000", R.drawable.gudeg));
         listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Kerak Telor", "Makanan khas DKI Jakarta", "Rp. 20.000", R.drawable.kerak_telor));
         listKuliner.add(new com.example.aplikasimenumakanan.Kuliner("Sate Bandeng", "Makanan khas Banten", "Rp. 30.000", R.drawable.sate_bandeng));
